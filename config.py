7e338854aa313dea762cf81ce8046a65c8857006
BeingAnonymous4u <155914718+BeingAnonymous4u@users.noreply.github.com>
2024-08-16 02:47:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,13 +18,13 @@ def is_enabled(value, default):
         return default
       
 # Bot Information
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
+API_ID = int(environ.get("API_ID", "21207644"))
+API_HASH = environ.get("API_HASH", "0143a7ea5d697fc0c0c5a4899b5b9314")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
-BOT_USERNAME = environ.get("BOT_USERNAME", "") # without @
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6941610925').split()]
+BOT_USERNAME = environ.get("BOT_USERNAME", "iPopCornstorebot") # without @
 PORT = environ.get("PORT", "8080")
 
 # Clone Info :-
