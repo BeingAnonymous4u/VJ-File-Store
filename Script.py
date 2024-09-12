9950cac1289cc2953e5efd9d98d3b5f6094baf33
BeingAnonymous4u <155914718+BeingAnonymous4u@users.noreply.github.com>
2024-09-13 01:53:53 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -13,7 +13,7 @@ class script(object):
 # Ask Doubt on telegram @KingVJ01
 
     
-    CAPTION = """<b>📂 ғɪʟᴇɴᴀᴍᴇ : {file_name}
+    CAPTION = """<b>📂 ғɪʟᴇɴᴀᴍᴇ :{file_name}
 
 sɪᴢᴇ 📽️: {file_size}
 </b>""" 
