9152eabe05fa88d893c91b9122f669fce33e7787
BeingAnonymous4u <155914718+BeingAnonymous4u@users.noreply.github.com>
2024-09-13 19:55:43 +0530

    Update genlink.py

--- plugins/genlink.py
+++ plugins/genlink.py
@@ -50,7 +50,7 @@ async def incoming_gen_link(bot, message):
         short_link = await get_short_link(user, share_link)
         await message.reply(f"<b>⭕ ʜᴇʀᴇ ɪs ʏᴏᴜʀ ʟɪɴᴋ:\n\n🖇️ sʜᴏʀᴛ ʟɪɴᴋ :- {short_link}</b>")
     else:
-        await message.reply(f"<b>⭕ ʜᴇʀᴇ ɪs ʏᴏᴜʀ ʟɪɴᴋ:\n\n🔗 ᴏʀɪɢɪɴᴀʟ ʟɪɴᴋ :- {share_link} \n\n🖇️ 𝙲𝚘𝚙𝚢 𝙷𝚎𝚛𝚎``` {share_link} ```</b>")
+        await message.reply(f"<b>⭕ ʜᴇʀᴇ ɪs ʏᴏᴜʀ ʟɪɴᴋ:\n\n🔗 ᴏʀɪɢɪɴᴀʟ ʟɪɴᴋ :- {share_link} \n\n🖇️ 𝙲𝚘𝚙𝚢 𝙷𝚎𝚛𝚎 <code>{share_link}</code></b>")
         
 
 @Client.on_message(filters.command(['link', 'plink']) & filters.create(allowed))
@@ -83,7 +83,7 @@ async def gen_link_s(bot, message):
         short_link = await get_short_link(user, share_link)
         await message.reply(f"<b>⭕ ʜᴇʀᴇ ɪs ʏᴏᴜʀ ʟɪɴᴋ:\n\n🖇️ sʜᴏʀᴛ ʟɪɴᴋ :- {short_link}</b>")
     else:
-        await message.reply(f"<b>⭕ ʜᴇʀᴇ ɪs ʏᴏᴜʀ ʟɪɴᴋ:\n\n🔗 ᴏʀɪɢɪɴᴀʟ ʟɪɴᴋ :- {share_link} \n\n🖇️ 𝙲𝚘𝚙𝚢 𝙷𝚎𝚛𝚎``` {share_link} ```</b>")
+        await message.reply(f"<b>⭕ ʜᴇʀᴇ ɪs ʏᴏᴜʀ ʟɪɴᴋ:\n\n🔗 ᴏʀɪɢɪɴᴀʟ ʟɪɴᴋ :- {share_link} \n\n🖇️ 𝙲𝚘𝚙𝚢 𝙷𝚎𝚛𝚎 <code>{share_link}</code></b>")
         
 
 # Don't Remove Credit Tg - @VJ_Botz
@@ -198,7 +198,7 @@ async def gen_link_batch(bot, message):
         short_link = await get_short_link(user, share_link)
         await sts.edit(f"<b>⭕ ʜᴇʀᴇ ɪs ʏᴏᴜʀ ʟɪɴᴋ:\n\nContains `{og_msg}` files.\n\n🖇️ sʜᴏʀᴛ ʟɪɴᴋ :- {short_link}</b>")
     else:
-        await sts.edit(f"<b>⭕ ʜᴇʀᴇ ɪs ʏᴏᴜʀ ʟɪɴᴋ:\n\nContains `{og_msg}` files.\n\n🔗 ᴏʀɪɢɪɴᴀʟ ʟɪɴᴋ :- {share_link} \n\n🖇️ 𝙲𝚘𝚙𝚢 𝙷𝚎𝚛𝚎``` {share_link} ```</b>")
+        await sts.edit(f"<b>⭕ ʜᴇʀᴇ ɪs ʏᴏᴜʀ ʟɪɴᴋ:\n\nContains `{og_msg}` files.\n\n🔗 ᴏʀɪɢɪɴᴀʟ ʟɪɴᴋ :- {share_link} \n\n🖇️ 𝙲𝚘𝚙𝚢 𝙷𝚎𝚛𝚎 <code>{share_link}</code></b>")
         
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
